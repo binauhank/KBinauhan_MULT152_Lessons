cd2ec231642ee961b751a03ad1a86cf4795fe733
binauhank <binauhankr@gmail.com>
2024-11-03 16:36:19 -0800

    Finished Unit 4: Lesson D

--- KBinauhan_Unit4/Assets/Scripts/Enemy.cs
+++ KBinauhan_Unit4/Assets/Scripts/Enemy.cs
@@ -7,6 +7,8 @@ public class Enemy : MonoBehaviour
     Rigidbody enemyRB;
     GameObject player;
     public float speed = 4.0f;
+
+    private float yBoundary = -15.0f;
     
     void Start()
     {
@@ -16,6 +18,10 @@ public class Enemy : MonoBehaviour
     
     void Update()
     {
+        if (transform.position.y < yBoundary)
+        {
+            Destroy(gameObject);
+        }
         Vector3 seekDirection = (player.transform.position - transform.position).normalized;
         enemyRB.AddForce(seekDirection * speed * Time.deltaTime);
     }
--- KBinauhan_Unit4/Assets/Scripts/SpawnManager.cs
+++ KBinauhan_Unit4/Assets/Scripts/SpawnManager.cs
@@ -5,11 +5,34 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public GameObject powerUpPrefab;
     private float spawnRange = 8.5f;
+    private int enemyCount;
+
+    private int waveNumber = 1;
     
     void Start()
     {
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        SpawnWave(waveNumber);
+    }
+
+    private void Update()
+    {
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        if (enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnWave(waveNumber);
+        }
+    }
+
+    void SpawnWave(int enemyNum)
+    {
+        Instantiate(powerUpPrefab, GenerateSpawnPosition(), powerUpPrefab.transform.rotation);
+        for (int i = 0; i < enemyNum; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
     }
 
     Vector3 GenerateSpawnPosition()
