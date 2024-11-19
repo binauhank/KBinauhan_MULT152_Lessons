e41b6474f86212587b8156e2f2adddb275afc6ac
binauhank <binauhankr@gmail.com>
2024-11-18 18:35:15 -0800

    Finished Unit 5: Lesson B

--- KBinauhan_Unit5/Assets/Scripts/GameManager.cs
+++ KBinauhan_Unit5/Assets/Scripts/GameManager.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     private const float spawnRate = 2.0f;
     public List<GameObject> prefabs;
+
+    public TextMeshProUGUI scoreText;
+    private int score = 0;
     
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        UpdateScore(0);
     }
 
     IEnumerator SpawnTarget()
@@ -19,5 +24,15 @@ public class GameManager : MonoBehaviour
             yield return new WaitForSeconds(spawnRate);
             Instantiate(prefabs[Random.Range(0, prefabs.Count)]);
         }
+    }
+
+    public void UpdateScore(int scoreDelta)
+    {
+        score += scoreDelta;
+        if (score < 0)
+        {
+            score = 0;
+        }
+        scoreText.text = "Score: " + score;
     }
 }
